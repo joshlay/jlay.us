8a201dfdabadf3588043f908b43e6ee64dc3205e
Josh Lay <me@jlay.us>
2026-09-19 12:40:14 -0500

    push.sh: drop useless dir comment

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DEST_HOST="${DEST_HOST:-jlay.us}"
-DEST_DIR="${DEST_DIR:-/srv/jlay.us/}"  # was /srv/nginx/html/ at hetzner
+DEST_DIR="${DEST_DIR:-/srv/jlay.us/}"
 source venv/bin/activate && mkdocs build && rsync -avP ./site/ "$DEST_HOST":"$DEST_DIR"
